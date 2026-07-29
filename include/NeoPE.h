fcc7085ac98c653af11f4e2f7fdb5a0d884dc9e5
axxo1337 <58981352+axxo1337@users.noreply.github.com>
2026-07-29 13:51:57 -0400

    feat: Signature

--- include/NeoPE.h
+++ include/NeoPE.h
@@ -1,6 +1,14 @@
+//  _  _         ___ ___ 
+// | \| |___ ___| _ \ __|  Header-only and lightweight PE utility library.
+// | .` / -_) _ \  _/ _|   version 0.0.0
+// |_|\_\___\___/_| |___|  https://github.com/pagefaultcc/NeoPE
+//
+// SPDX-FileCopyrightText. 2026 - 2027 pagefault.cc. <https://github.com/pagefaultcc>     
+// SPDX-License-Identifier: MIT            
+
 #pragma once
 
 namespace NeoPE
 {
-    
+
 }
